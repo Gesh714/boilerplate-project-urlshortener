c879210ad21812ec393d28e86d99c51ee9e59772
Gesh714 <kinegsanchezh@gmail.com>
2023-04-18 18:27:44 -0400

    final commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db')
 const app = express();
+const url = require('url');
 
-//conectar a la base de datos
-connectDB();
-
-app.use(express.json({ extended: false }));
-
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+app.use(express.urlencoded({ extended: true }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,6 +16,53 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+const originalUrls = [];
+const shortUrls = [];
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  if(!validarUrl(url)){
+    return res.json({
+      "error":"invalid url"
+    });
+  }
+  const foundIndex = originalUrls.indexOf(url);
+
+  if (foundIndex < 0) {
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1
+    });
+  }
+  return res.json({
+    originalUrls: url,
+    short_url: shortUrls[foundIndex]
+  });
+});
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const shorturl = parseInt(req.params.shorturl);
+  const foundIndex = shortUrls.indexOf(shorturl);
+   if(foundIndex < 0){
+    return res.json({
+      "error":"No short URL found for the given input"
+    });
+   } 
+   res.redirect(originalUrls[foundIndex]);
+});
+
+const validarUrl = (url) => {
+  try {
+    const urlObj = new URL(url);
+    return urlObj.protocol === 'http:' || urlObj.protocol === 'https:';
+  } catch (error) {
+    return false;
+  }
+}
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
